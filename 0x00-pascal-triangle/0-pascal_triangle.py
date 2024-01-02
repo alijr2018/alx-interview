ff1eae732f4489594fbafaf03b51bd1ec9bdb15a
alijr2018 <alijr_2018@proton.me>
2024-01-02 10:44:44 +0100

    first challenge done

---
+++ 0x00-pascal-triangle/0-pascal_triangle.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+0-pascal_triangle.py
+"""
+from math import factorial
+
+
+def pascal_triangle(n):
+    """
+    this function returns a list of lists of integers representing the Pascal’s triangle of n
+    """
+    if n <= 0:
+        return []
+
+    triangles = []
+    for i in range(n):
+        r = [1] * (i + 1)
+        if i >= 2:
+            for j in range(1, i):
+                r[j] = triangles[i - 1][j - 1] + triangles[i - 1][j]
+        triangles.append(r)
+    return triangle

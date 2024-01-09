d3573a7d9609038ba7b577f68161527886673c4c
alijr2018 <alijr_2018@proton.me>
2024-01-09 20:18:10 +0100

    try

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -2,5 +2,23 @@
 """
 0-lockboxes.py
 """
+from collections import deque
+
 
 def canUnlockAll(boxes):
+    if not boxes or len(boxes) == 0:
+        return False
+
+    opened_boxes = set()
+    opened_boxes.add(0)
+
+    queue = deque([0])
+    while queue:
+        current_box = queue.popleft()
+
+        for key in boxes[current_box]:
+            if key < len(boxes) and key not in opened_boxes:
+                opened_boxes.add(key)
+                queue.append(key)
+
+        return len(opened_boxes) == len(boxes)

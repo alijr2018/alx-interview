3f32da4abca391defb4f31315317c61a4c8ee50c
alijr2018 <alijr_2018@proton.me>
2024-01-16 20:26:15 +0100

    !fixed

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -11,10 +11,8 @@ def minOperations(n):
     if n <= 1:
         return 0
 
-    # Find the largest factor of n
     for i in range(2, int(n**0.5) + 1):
         if n % i == 0:
             return minOperations(n // i) + i
 
-    # If n is a prime number, it can only be achieved by copying and pasting
     return n

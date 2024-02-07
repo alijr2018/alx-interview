3ce68c3df4f73eae4957212809ce3380a31fb4ae
alijr2018 <alijr_2018@proton.me>
2024-02-07 10:32:51 +0100

    finally

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,1 +1,67 @@
-#!/usr/bin/python3x
+#!/usr/bin/python3
+"""
+0-nqueens.py
+"""
+import sys
+
+
+def is_safe(board, row, col, n):
+    for i in range(col):
+        if board[row][i] == 1:
+            return False
+
+    for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
+        if board[i][j] == 1:
+            return False
+
+    for i, j in zip(range(row, n, 1), range(col, -1, -1)):
+        if board[i][j] == 1:
+            return False
+
+    return True
+
+
+def solve_nqueens_util(board, col, n):
+    if col >= n:
+        print_board(board)
+        return True
+
+    res = False
+    for i in range(n):
+        if is_safe(board, i, col, n):
+            board[i][col] = 1
+            res = solve_nqueens_util(board, col + 1, n) or res
+            board[i][col] = 0
+    return res
+
+
+def solve_nqueens(n):
+    if not isinstance(n, int):
+        print("N must be a number")
+        sys.exit(1)
+
+    if n < 4:
+        print("N must be at least 4")
+        sys.exit(1)
+
+    board = [[0] * n for _ in range(n)]
+    if not solve_nqueens_util(board, 0, n):
+        print(f"No solution for {n} queens")
+
+
+def print_board(board):
+    result = [[i, board[i].index(1)] for i in range(len(board))]
+    print(result)
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: nqueens N")
+        sys.exit(1)
+
+    try:
+        N = int(sys.argv[1])
+        solve_nqueens(N)
+    except ValueError:
+        print("N must be a number")
+        sys.exit(1)

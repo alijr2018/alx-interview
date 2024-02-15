28ce2f315a94280ec6ba248250eee6a3095bdd6c
alijr2018 <alijr_2018@proton.me>
2024-02-15 11:16:41 +0100

    checking

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,20 +1,26 @@
 #!/usr/bin/node
 
-const axios = require('axios');
+const request = require('request');
 const movieId = process.argv[2];
 
 const apiUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}/`;
 
-axios.get(apiUrl)
-  .then(response => {
-    const filmData = response.data;
-    const characters = filmData.characters;
-
-    characters.forEach(characterUrl => {
-      axios.get(characterUrl)
-        .then(response => {
-          const characterData = response.data;
-          console.log(characterData.name);
+request(apiUrl, async function (error, response, body) {
+  if (error) {
+    return console.log(error);
+  } else {
+    const characters = JSON.parse(body).characters;
+    for (const character in characters) {
+      const res = await new Promise((resolve, reject) => {
+        request(characters[character], (err, res, html) => {
+          if (err) {
+            reject(err);
+          } else {
+            resolve(JSON.parse(html).name);
+          }
         });
-    });
-  });
+      });
+      console.log(res);
+    }
+  }
+});

0b9b2068fa59fe4fef3081b5bb75766b54eb9fec
alijr2018 <alijr_2018@proton.me>
2024-02-15 10:58:20 +0100

    check

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -10,24 +10,11 @@ axios.get(apiUrl)
     const filmData = response.data;
     const characters = filmData.characters;
 
-    if (!characters || characters.length === 0) {
-      console.error('No characters found for the specified movie ID.');
-      process.exit(1);
-    }
-
     characters.forEach(characterUrl => {
       axios.get(characterUrl)
         .then(response => {
           const characterData = response.data;
           console.log(characterData.name);
-        })
-        .catch(error => {
-          console.error('Error fetching character data:', error.message);
-          process.exit(1);
         });
     });
-  })
-  .catch(error => {
-    console.error('Error fetching data from the Star Wars API:', error.message);
-    process.exit(1);
   });

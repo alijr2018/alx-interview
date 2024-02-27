d1705bff72c2b48fd758231e4691434d41b4bd22
alijr2018 <alijr_2018@proton.me>
2024-02-27 13:08:07 +0100

    checkin gif done

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+Module for making change using dynamic programming
+"""
+
+
+def makeChange(coins, total):
+    """
+    Determine the fewest number of coins needed to meet a given amount total.
+    """
+    if total < 0:
+        return -1
+
+    dp = [float('inf')] * (total + 1)
+    dp[0] = 0
+
+    for coin in coins:
+        for amount in range(coin, total + 1):
+            dp[amount] = min(dp[amount], dp[amount - coin] + 1)
+
+    return dp[total] if dp[total] != float('inf') else -1

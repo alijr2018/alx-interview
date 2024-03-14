271215b4f06bdab12d45d152cd1fd583fc94c4f2
alijr2018 <alijr_2018@proton.me>
2024-03-14 14:17:28 +0000

    testing

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -4,59 +4,40 @@
 """
 
 
+def is_prime(num):
+    """
+    checking is it prime
+    """
+    prime_numbers = []
+    sieve = [True] * (num + 1)
+    for number in range(2, num + 1):
+        if sieve[number]:
+            prime_numbers.append(number)
+            for multiple in range(number * number, num + 1, number):
+                sieve[multiple] = False
+    return prime_numbers
+
+
 def isWinner(x, nums):
-    def is_prime(num):
-        if num <= 1:
-            return False
-        if num <= 3:
-            return True
-        if num % 2 == 0 or num % 3 == 0:
-            return False
-        i = 5
-        while i * i <= num:
-            if num % i == 0 or num % (i + 2) == 0:
-                return False
-            i += 6
-        return True
-
-    def calculate_primes(n):
-        primes = []
-        for i in range(2, n + 1):
-            if is_prime(i):
-                primes.append(i)
-        return primes
-
-    def can_player_win(primes, n):
-        memo = {}
-
-        def can_win(count):
-            if count <= 1:
-                return False
-            if count in memo:
-                return memo[count]
-            for prime in primes:
-                if count % prime == 0:
-                    if not can_win(count - prime):
-                        memo[count] = True
-                        return True
-            memo[count] = False
-            return False
-
-        return can_win(n)
-
+    """
+    is winner
+    """
+    if x is None or nums is None or x == 0 or not nums:
+        return None
+    
     maria_wins = 0
     ben_wins = 0
-
-    for n in nums:
-        primes = calculate_primes(n)
-        if can_player_win(primes, n):
-            maria_wins += 1
-        else:
+    
+    for limit in nums:
+        prime_numbers = is_prime(limit)
+        if len(prime_numbers) % 2 == 0:
             ben_wins += 1
-
+        else:
+            maria_wins += 1
+    
     if maria_wins > ben_wins:
-        return "Maria"
-    elif maria_wins < ben_wins:
-        return "Ben"
+        return 'Maria'
+    elif ben_wins > maria_wins:
+        return 'Ben'
     else:
         return None
